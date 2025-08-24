dfe16df7690d8fd5c584a724c8b3c2fe0e0fe6ae
Lohran Victor <lohran.hps@gmail.com>
2025-08-23 23:16:38 -0300

    feat: `Added new endpoint for OS details and updated template to display additional information`

--- GO/views.py
+++ GO/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from .models import OrdemServico
 from django import forms
@@ -18,13 +19,43 @@ def lista_servicos(request):
             os_instance.codigo_os = f"{os_instance.numero_os}{os_instance.tag}"
             
             os_instance.save()
-            return redirect('home')  # Redirect to home after saving
+            return redirect('home')
 
     else:
         form = OrdemServicoForm()
 
     servicos = OrdemServico.objects.all()
     return render(request, 'home.html', {'servicos': servicos, 'form': form})
+
+def detalhes_os(request, os_id):
+    try:
+        os_instance = OrdemServico.objects.get(id=os_id)
+        data = {
+            'id': os_instance.id,
+            'numero_os': os_instance.numero_os,
+            'tag': os_instance.tag,
+            'codigo_os': os_instance.codigo_os,
+            'data_inicio': os_instance.data_inicio.strftime('%d/%m/%Y') if os_instance.data_inicio else '',
+            'data_fim': os_instance.data_fim.strftime('%d/%m/%Y') if os_instance.data_fim else '',
+            'dias_de_operacao': os_instance.dias_de_operacao,
+            'cliente': os_instance.cliente,
+            'unidade': os_instance.unidade,
+            'solicitante': os_instance.solicitante,
+            'tipo_operacao': os_instance.tipo_operacao,
+            'servico': os_instance.servico,
+            'metodo': os_instance.metodo,
+            'tanque': os_instance.tanque,
+            'volume_tanque': str(os_instance.volume_tanque),
+            'especificacao': os_instance.especificacao,
+            'pob': os_instance.pob,
+            'coordenador': os_instance.coordenador,
+            'supervisor': os_instance.supervisor,
+            'status_operacao': os_instance.status_operacao,
+            'observacao': os_instance.observacao,
+        }
+        return JsonResponse(data)
+    except OrdemServico.DoesNotExist:
+        return JsonResponse({'error': 'Ordem de Serviço não encontrada.'}, status=404)
 
 class OrdemServicoForm(forms.ModelForm):
     NOVA_OS = 'nova'
