406985bb6049f1b6f1bbbbb460ddb4df4f2cdd49
gabrielandrade131 <gabrielandrade131@gmail.com>
2025-08-25 20:36:01 -0300

    refactor(forms): Refactor OrdemServicoForm to include OS type selection; update views and templates

--- GO/forms.py
+++ GO/forms.py
@@ -2,9 +2,17 @@ from django import forms
 from .models import OrdemServico
 
 class OrdemServicoForm(forms.ModelForm):
-    class preenchimento:
+    NOVA_OS = 'nova'
+    EXISTENTE_OS = 'existente'
+    BOX_CHOICES = [
+        (NOVA_OS, 'Nova OS'),
+        (EXISTENTE_OS, 'OS já existente'),
+    ]
+    box_opcao = forms.ChoiceField(choices=BOX_CHOICES, widget=forms.RadioSelect, label="Tipo de OS", initial='nova')
+
+    class Meta:
         model = OrdemServico
-        exclude = ['codigo_os', 'dias_de_operação']
+        exclude = ['codigo_os', 'dias_de_operacao']
         widgets = {
             'tag': forms.Select(attrs={'class': 'form-control'}),
             'numero_os': forms.NumberInput(attrs={'class': 'form-control'}),
@@ -17,7 +25,7 @@ class OrdemServicoForm(forms.ModelForm):
             'pob': forms.NumberInput(attrs={'class': 'form-control'}),
             'tanque': forms.TextInput(attrs={'class': 'form-control'}),
             'volume_tanque': forms.NumberInput(attrs={'class': 'form-control', 'step': '0.01'}),
-            'cliente': forms.TextInput(attrs={'class': 'form-control'}),
+            'cliente': forms.Select(attrs={'class': 'form-control'}),
             'unidade': forms.TextInput(attrs={'class': 'form-control'}),
             'tipo_operacao': forms.Select(attrs={'class': 'form-control'}),
             'solicitante': forms.TextInput(attrs={'class': 'form-control'}),
@@ -27,3 +35,20 @@ class OrdemServicoForm(forms.ModelForm):
             'link_rdo': forms.URLInput(attrs={'class': 'form-control'}),
             'detalhes': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
         }
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        box_opcao = self.cleaned_data.get('box_opcao')
+        
+        if box_opcao == self.NOVA_OS:
+            # For new OS, generate next number
+            ultimo = OrdemServico.objects.order_by('-numero_os').first()
+            instance.numero_os = (ultimo.numero_os + 1) if ultimo else 1
+        elif box_opcao == self.EXISTENTE_OS:
+            # For existing OS, keep the same number as the last one
+            ultimo = OrdemServico.objects.order_by('-numero_os').first()
+            instance.numero_os = ultimo.numero_os if ultimo else 1
+        
+        if commit:
+            instance.save()
+        return instance
